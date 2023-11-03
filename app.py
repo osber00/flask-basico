e878b1c785c55d8c4e64d78d80b628604b2af486
Oscar Bertel P <osber00@gmail.com>
2023-11-03 12:54:18 -0500

    Básico para deploy

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+if __name__ == '__main__':
+    app.run()
