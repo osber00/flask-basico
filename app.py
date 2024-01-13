064d6b5e7abc2a91678c89c227b1b4800c5300e6
Oscar Bertel P <osber00@gmail.com>
2024-01-12 23:12:36 -0500

    Archivos estáticos

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
-app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECRET_KEY'] = 'mysecret2'
 
 @app.route('/')
 def index():
